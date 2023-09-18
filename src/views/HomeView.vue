b781a562b2af41bdb67f62eb6c1657491e51b279
Sudiptya <chandasudiptya@gmail.com>
2023-09-19 01:52:33 +0600

    adding files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Asho Random Number er square Shikhi :3 </h1>
+    <div class="counter">
+      {{ $store.state.counter }}
+    </div>
+    <div class="counter-squared">
+      {{ $store.state.counter }}
+      <sup>2</sup> =
+      {{ $store.getters.counterSquared }}
+    </div>
+    <div class="buttons">
+      <button @click="$store.dispatch ('decreaseCounter')">-</button>
+      <button @click="$store.dispatch ('increaseCounter')">+</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
+
 </script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
